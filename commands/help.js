ef3bfdcccef4928db466a22fd977bc85f8163907
5000 .0 <dragonlivesm@gmail.com>
2019-11-21 15:33:28 +0000

    changed token, help.js

--- commands/help.js
+++ commands/help.js
@@ -27,26 +27,6 @@ module.exports = {
 					console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
 					message.reply('it seems like I can\'t DM you!');
 				});
-		}
-
-		if (args[0]!==undefined){
-			const name = args[0].toLowerCase()
-			const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-			if (!command) {
-				return message.reply('that\'s not a valid command / that\'s not a command!');
-			}
-	
-			data.push(`**Name:** ${command.name}`);
-	
-			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-			if (command.description) data.push(`**Description:** ${command.description}`);
-			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-	
-			data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-	
-			message.channel.send(data, { split: true });
-		}if (args===undefined){
-			return;
 		}	
 	},
 }

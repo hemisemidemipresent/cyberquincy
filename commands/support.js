271b45ea37023af5fc5f8823a6b5c6ffd07bf1cd
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2019-12-02 11:55:36 +0800

    added support.js

---
+++ commands/support.js
@@ -0,0 +1,8 @@
+module.exports ={
+    "name":"support",
+    "aliases":["email"],
+    execute(message,args){
+        message.delete()
+        message.channel.send('support@ninjakiwi.com')
+    }
+}

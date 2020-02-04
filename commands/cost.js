c6757f95665366e573d02d4d7e9dd48b5f19bf1c
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-02-04 21:57:26 +0800

    df

--- commands/cost.js
+++ commands/cost.js
@@ -1,57 +1,92 @@
 const Discord = require('discord.js');
+const t = require('../towers.json')
+let to = ['dart', 'boomer', 'tack', 'glue', 'ice', 'bomb', 'sniper', 'heli', 'ace', 'sub', 'boat', 'mortar', 'ninja', 'super', 'wizard', 'alch', 'druid', 'engi', 'farm', 'spac', 'village']
 module.exports = {
 	name: 'cost',
 	aliases: ['price', 'convert'],
 	execute(message, args, client) {
-		if (!args) return message.channel.send('The syntax is q!cost <money> <gamemode>');
-		let yeet, cost, diff;
-		if (!isNaN(args[0])) {
-			cost = args[0]
-		} else {
-			if (args[0].includes('ea')) {
-				yeet = cost / 0.85;
-				diff = 'easy'
-			} else if (args[0].includes('me') || args[0].includes('no')) {
-				yeet = cost;
-				diff = 'normal'
-			} else if (args[0].includes('ha')) {
-				yeet = cost / 1.08;
-				diff = 'hard'
-			} else if (args[0].includes('im')) {
-				yeet = cost / 1.2;
-				diff = 'impoppable'
-			} else {
-				if (!isNaN(args[1])) {
-					cost = args[1]
-				}
-				if (args[1].includes('ea')) {
-					yeet = cost / 0.85;
-					diff = 'easy'
-				} else if (args[1].includes('me') || args[1].includes('no')) {
-					yeet = cost;
-					diff = 'normal'
-				} else if (args[1].includes('ha')) {
-					yeet = cost / 1.08;
-					diff = 'hard'
-				} else if (args[1].includes('im')) {
-					yeet = cost / 1.2;
-					diff = 'impoppable'
-				} else {
-					return message.channel.send('The syntax is q!cost <money> <gamemode>');
-				}
-			}
+		const filter = msg => msg.author.id === `${message.author.id}`;
+		message.channel.send('Type the option number in chat:\n1. convert cost from a mode to another mode\n2. find the cost of a tower')
+		message.channel.awaitMessages(filter, { maxMatches: 1, time: 10000, errors: ["time"] }).then(a => {
+			let option = a.first().content;
+			//convert monays
+			if (option == 1) {
+				return message.channel.send('sorry, this option is under repair')
+				/*message.channel.send('what is the mode of the cash that you are converting from?\n1.easy\n2.normal\n3.hard\n4.impoppable')
+				message.channel.awaitMessages(filter, { maxMatches: 1, time: 10000, errors: ["time"] }).then(b => {
+					let mode = b.first().content;
+					message.channel.send('Please type the cash you want to convert in chat')
+					message.channel.awaitMessages(filter, { maxMatches: 1, time: 10000, errors: ["time"] }).then(c => {
+						let money = c.first().content;
 
-		}
-		let easy = Math.floor(yeet * 0.85);
-		let normal = Math.floor(yeet);
-		let hard = Math.floor(yeet * 1.08);
-		let impoppable = Math.floor(yeet * 1.2);
-		const embed = new Discord.RichEmbed()
-			.setDescription(`converted ${cost} in ${diff} game mode:`)
-			.addField('easy', `${easy}`, true)
-			.addField('normal', `${normal}`, true)
-			.addField('hard', `${hard}`, true)
-			.addField('impoppable', `${impoppable}`, true);
-		message.channel.send(embed);
+						if (mode == 1) n = money / 0.85 / 5
+						else if (mode == 2) n = money / 5
+						else if (mode == 2) n = money / 1.08 / 5
+						else if (mode == 2) n = money / 1.2 / 5
+						let easy = n * 0.85
+						let norm = n
+						let hard = n * 1.08
+						let impo = n * 1.2
+						let arr = []
+						arr.push(Math.round(easy) * 5)
+						arr.push(Math.round(norm) * 5)
+						arr.push(Math.round(hard) * 5)
+						arr.push(Math.round(impo) * 5)
+						const embed = new Discord.RichEmbed()
+							.addField('easy', `${arr[0]}`, true)
+							.addField('normal', `${arr[1]}`, true)
+							.addField('hard', `${arr[1]}`, true)
+							.addField('impoppable', `${arr[3]}`, true);
+						message.channel.send(embed)
+					})
+				})*/
+			} else if (option == 2) {
+				let fembed = new Discord.RichEmbed()
+					.setDescription('please type the tower **number** you want to find the cost of:')
+				for (i = 0; i < to.length; i++) {
+					fembed.addField(i, to[i], true)
+				}
+				message.channel.send(fembed)
+				message.channel.awaitMessages(filter, { maxMatches: 1, time: 10000, errors: ["time"] }).then(tt => {
+					let tu = tt.first().content
+					let tower = to[tu]
+					if (isNaN(tu) || tu < 0 || tu > 20) return message.channel.send('invalid input')
+					message.channel.send('please specify the path; e.g. 003')
+					message.channel.awaitMessages(filter, { maxMatches: 1, time: 10000, errors: ["time"] }).then(p => {
+						let pr = p.first().content
+						let path1 = Math.floor(parseInt(pr) / 100);
+						let path2 = Math.floor((parseInt(pr) - path1 * 100) / 10);
+						let path3 = parseInt(pr) - path1 * 100 - path2 * 10;
+						if (path1 > 5 || path1 < 0 || path2 > 5 || path2 < 0 || path3 > 5 || path3 < 0) {
+							return message.channel.send('please specify a proper path.')
+						}
+						let costs = []
+						if (path1 !== 0) {
+							let e = 0
+							for (j = 1; j < path1 + 1; j++) {
+								e += parseInt(t[tower][`s${1}${j}`]['cost'])
+							}
+							costs.push(e)
+						} else costs.push('0')
+						if (path2 !== 0) {
+							let e = 0
+							for (j = 1; j < path2 + 1; j++) {
+								e += parseInt(t[tower][`s${2}${j}`]['cost'])
+							}
+							costs.push(e)
+						} else costs.push('0')
+						if (path3 !== 0) {
+							let e = 0
+							for (j = 1; j < path3 + 1; j++) {
+								e += parseInt(t[tower][`s${3}${j}`]['cost'])
+								console.log(`${e}, ${parseInt(t[tower][`s${3}${j}`]['cost'])}`)
+							}
+							costs.push(e)
+						} else costs.push('0')
+						message.channel.send(`The total cost is (including base tower cost) is ${parseInt(costs[0]) + parseInt(costs[1]) + parseInt(costs[2]) + parseInt(t[tower].base.cost)} (on medium)`)
+					})
+				})
+			}
+		})
 	}
 };

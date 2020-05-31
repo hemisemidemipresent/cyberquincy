fda4c2a9953aeee8acd6f0d8ce485402b96dcf25
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-05-31 15:59:47 +0800

    changed tick emoji

--- commands/dart.js
+++ commands/dart.js
@@ -38,7 +38,7 @@ module.exports = {
             const embed = new Discord.MessageEmbed()
                 .setTitle('Please choose one upgrade at a time!')
                 .setDescription(
-                    `For example:\n❌ q!${name} 130\n✔️ q!${name} 030`
+                    `For example:\n❌ q!${name} 130\n✅ q!${name} 030`
                 );
             return message.channel.send(embed);
         }
--- commands/herolevel.js
+++ commands/herolevel.js
@@ -4,6 +4,29 @@ module.exports = {
     name: 'herolevel',
     aliases: ['hero', 'hl'],
     execute(message, args, client) {
+        let xp_per_level = [
+            0,
+            0,
+            180,
+            460,
+            1000,
+            1860,
+            3280,
+            5180,
+            8320,
+            9380,
+            13620,
+            16380,
+            14400,
+            16650,
+            14940,
+            16380,
+            17820,
+            19260,
+            20700,
+            16470,
+            17280,
+        ];
         const heroes = [
             'Quincy',
             'Gwen',
@@ -14,81 +37,73 @@ module.exports = {
             'Churchill',
             'Pat',
             'Adora',
-            'Brickwell',
+            'Brickell',
         ];
         const hero_xp_curves = [1, 1, 1, 1, 1.425, 1.5, 1.8, 1.425, 1.8, 1];
         function level_cal(B16, xp_slope, diff_mult, heroname) {
-            let xp_per_level = [
-                0,
-                0,
-                180,
-                460,
-                1000,
-                1860,
-                3280,
-                5180,
-                8320,
-                9380,
-                13620,
-                16380,
-                14400,
-                16650,
-                14940,
-                16380,
-                17820,
-                19260,
-                20700,
-                16470,
-                17280,
-            ]; //xp per hero level
+            //xp per hero level
+
+            //honestly all of these are sorcery that I dont know what they do but I wrote this spaghetti so i will fix the spaghetti
             let D14 = Math.floor(B16 * diff_mult); //round8, level 1 price
             let D15 = D14 - 2 * 20 * diff_mult; //round9, level 1 price
             let p = [0, 0]; //0s are placeholder numbers to occupy indexing space
             for (i = 2; i < 21; i++) {
                 p.push(Math.ceil(xp_per_level[i] * xp_slope)); //xp_slope is the xp "slope", i.e. how much xp needed per level multiplier
             }
-            let y = [0, 0]; //y is the sum of all the previosu entries of p
-            let sp = 0;
+            let y = [0, 0]; //y is the sum of all the previous numbers of p
+            let temp = 0; // temp var.
             for (i = 2; i < 21; i++) {
                 for (j = 2; j < i + 1; j++) {
-                    sp = sp + p[j];
+                    temp += p[j];
                 }
-                y.push(sp);
-                sp = 0;
+                y.push(temp);
+                temp = 0;
             }
 
-            let r = [0, D14, D15]; //r is round xp given to hero
+            let xp_per_round = [0, D14, D15]; //r is xp per round (for the hero)
             for (i = 3; i < 22; i++) {
-                r.push(
-                    r[i - 1] -
-                        ((r[i - 2] - r[i - 1]) / diff_mult + 20) * diff_mult
+                xp_per_round.push(
+                    xp_per_round[i - 1] -
+                        ((xp_per_round[i - 2] - xp_per_round[i - 1]) /
+                            diff_mult +
+                            20) *
+                            diff_mult
                 );
             }
             for (i = 22; i < 52; i++) {
-                r.push(
-                    r[i - 1] -
-                        ((r[i - 2] - r[i - 1]) / diff_mult + 40) * diff_mult
+                xp_per_round.push(
+                    xp_per_round[i - 1] -
+                        ((xp_per_round[i - 2] - xp_per_round[i - 1]) /
+                            diff_mult +
+                            40) *
+                            diff_mult
                 );
             }
             for (i = 52; i < 102; i++) {
                 //might be broken
-                r.push(
-                    r[i - 1] -
-                        ((r[i - 2] - r[i - 1]) / diff_mult + 90) * diff_mult
+                xp_per_round.push(
+                    xp_per_round[i - 1] -
+                        ((xp_per_round[i - 2] - xp_per_round[i - 1]) /
+                            diff_mult +
+                            90) *
+                            diff_mult
                 );
             }
-            let ooo = []; //final result array
-            for (yeet = 1; yeet < 21; yeet++) {
-                let ix = 1; //ix is the round that the hero gets the level to
-                let reee = 1; //reee, and the while loop help by continuously increasing the xp until it can afford the level
-                while (reee > 0) {
-                    reee = r[ix] + y[yeet]; //yeet is the hero level number
-                    ix++;
+            let finalArr = []; //final result array
+            for (level = 1; level < 21; level++) {
+                let levelUpRound = 1; //levelUpRound is the round that the hero gets the level to
+                let heroCost = 1; //this is the temporary variable that stores the cost of upgrading the hero at a certain round
+                while (heroCost > 0) {
+                    // while the hero hasnt reached level "level" yet
+                    heroCost = xp_per_round[levelUpRound] + y[level]; //
+                    levelUpRound++;
                 }
-                if (ix > 100) {
-                    ooo.push('>100');
+                if (levelUpRound > 100) {
+                    // if the hero wont level up until round 100
+                    finalArr.push('>100');
                 } else {
-                    ooo.push(ix - 1);
+                    // edge error aaaaaaaaaaaa
+                    finalArr.push(levelUpRound - 1);
                 }
             }
             const embed = new Discord.MessageEmbed()
@@ -96,26 +111,26 @@ module.exports = {
                 .setDescription(
                     'This shows which round the hero will reach which level'
                 )
-                .addField('level 1', `r${ooo[0]}`, true)
-                .addField('level 2', `r${ooo[1]}`, true)
-                .addField('level 3', `r${ooo[2]}`, true)
-                .addField('level 4', `r${ooo[3]}`, true)
-                .addField('level 5', `r${ooo[4]}`, true)
-                .addField('level 6', `r${ooo[5]}`, true)
-                .addField('level 7', `r${ooo[6]}`, true)
-                .addField('level 8', `r${ooo[7]}`, true)
-                .addField('level 9', `r${ooo[8]}`, true)
-                .addField('level 10', `r${ooo[9]}`, true)
-                .addField('level 11', `r${ooo[10]}`, true)
-                .addField('level 12', `r${ooo[11]}`, true)
-                .addField('level 13', `r${ooo[12]}`, true)
-                .addField('level 14', `r${ooo[13]}`, true)
-                .addField('level 15', `r${ooo[14]}`, true)
-                .addField('level 16', `r${ooo[15]}`, true)
-                .addField('level 17', `r${ooo[16]}`, true)
-                .addField('level 18', `r${ooo[17]}`, true)
-                .addField('level 19', `r${ooo[18]}`, true)
-                .addField('level 20', `r${ooo[19]}`, true)
+                .addField('level 1', `r${finalArr[0]}`, true)
+                .addField('level 2', `r${finalArr[1]}`, true)
+                .addField('level 3', `r${finalArr[2]}`, true)
+                .addField('level 4', `r${finalArr[3]}`, true)
+                .addField('level 5', `r${finalArr[4]}`, true)
+                .addField('level 6', `r${finalArr[5]}`, true)
+                .addField('level 7', `r${finalArr[6]}`, true)
+                .addField('level 8', `r${finalArr[7]}`, true)
+                .addField('level 9', `r${finalArr[8]}`, true)
+                .addField('level 10', `r${finalArr[9]}`, true)
+                .addField('level 11', `r${finalArr[10]}`, true)
+                .addField('level 12', `r${finalArr[11]}`, true)
+                .addField('level 13', `r${finalArr[12]}`, true)
+                .addField('level 14', `r${finalArr[13]}`, true)
+                .addField('level 15', `r${finalArr[14]}`, true)
+                .addField('level 16', `r${finalArr[15]}`, true)
+                .addField('level 17', `r${finalArr[16]}`, true)
+                .addField('level 18', `r${finalArr[17]}`, true)
+                .addField('level 19', `r${finalArr[18]}`, true)
+                .addField('level 20', `r${finalArr[19]}`, true)
                 .setColor(colour);
             return embed;
         }
@@ -123,7 +138,7 @@ module.exports = {
         if (!args[0] && !args[1] && !args[2]) {
             message.channel
                 .send(
-                    'Please select hero and type the number into chat\n1 - quincy\n2 - gwen\n3 - obyn\n4 - jones\n5 - ezili\n6 - ben\n7 - churchill\n8 - pat\n9 - adora\n10 - brickwell'
+                    'Please select hero and type the number into chat\n1 - quincy\n2 - gwen\n3 - obyn\n4 - jones\n5 - ezili\n6 - ben\n7 - churchill\n8 - pat\n9 - adora\n10 - brickell'
                 )
                 .then(() => {
                     message.channel
@@ -135,7 +150,7 @@ module.exports = {
 
                         .then((collected) => {
                             let f = collected.first().content;
-                            if (isNaN(f) || f < 1 || f > 9) {
+                            if (isNaN(f) || f < 1 || f > 10) {
                                 return message.channel.send(
                                     'sorry, please specify a valid hero number next time. run the command again'
                                 );

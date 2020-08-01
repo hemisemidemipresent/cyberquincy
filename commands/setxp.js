5fd210b507dfabb4592cf0b6a5a0caa3bf0c44ca
rmlgaming <linden.cool@gmail.com>
2020-08-01 11:15:53 -0400

    Removals

--- commands/setxp.js
+++ commands/setxp.js
@@ -42,36 +42,6 @@ module.exports = {
         return message.channel.send(`Your xp is now ${tag.xp} making you level ${Xp.xpToLevel(tag.xp)}`);
     },
 
-    async displayStats(user, message) {
-        whose_xp = user.id === message.author.id ? "Your" : `${user.username}'s`
-        let tag = await Tags.findOne({ where: 
-            { 
-                id: user.id 
-            } 
-        });
-
-        // Create db user if it doesn't already exist
-        if (!tag) {
-            tag = await Tags.create({
-                id: user.id,
-                xp: 0, 
-            });
-        }
-
-        // Display level+xp
-        const xpEmbed = new Discord.MessageEmbed()
-            .setTitle(`${whose_xp} xp`)
-            .addField('level', Xp.xpToLevel(tag.xp))
-            .addField('xp', tag.xp)
-            .setColor(colours["cyber"])
-            .addField(
-                'Have a suggestion or found a bug?',
-                'Please tell us [here](https://discord.gg/VMX5hZA)!'
-            )
-            .setFooter('use `q!level rewards` to see role rewards');
-        return message.channel.send(xpEmbed);
-    },
-
     helpMessage(message) {
         const hembed = new Discord.MessageEmbed()
                 .setTitle("`q!setxp <xp>` Help")

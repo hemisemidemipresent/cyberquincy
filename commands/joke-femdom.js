20cdbbba5cf18fef51a814e76f53b1f7ef1b616f
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-08-27 07:07:39 +0800

    Update joke-femdom.js

--- commands/joke-femdom.js
+++ commands/joke-femdom.js
@@ -2,8 +2,10 @@ module.exports = {
     name: 'femdom',
     aliases: ['mochinator', 'moch'],
     execute(message) {
-        return message.channel.send(
-            'if I may offer input, ezili seems restrained rather than an actual bottom, if you get her to open up then she will absolutely destroy you\ngwen strikes me as the type to act tough on the field but enjoy cuddles and rainbows and stuff'
-        );
+        if (message.guild.id == 543957081183617024) {
+            return message.channel.send(
+                'if I may offer input, ezili seems restrained rather than an actual bottom, if you get her to open up then she will absolutely destroy you\ngwen strikes me as the type to act tough on the field but enjoy cuddles and rainbows and stuff'
+            );
+        } else return;
     },
 };

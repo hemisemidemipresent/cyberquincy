02bd5dcbe28846762dacb5098b23f97cbb25ed9a
rmlgaming <linden.cool@gmail.com>
2020-09-17 08:20:40 -0400

    Almost forgot

--- alias-repository.js
+++ alias-repository.js
@@ -238,10 +238,11 @@ module.exports = class AliasRepository extends Array {
     }
 
     towerUpgradeToIndexNormalForm(upgrade) {
-        const index_normal = this.getAliasSet(upgrade)[1];
-        return index_normal
-            .split('_')
-            .map((tk) => h.toTitleCase(tk))
-            .join(' ');
+        const index_normal_unformatted = this.getAliasSet(upgrade)[1];
+        return this.toIndexNormalForm(index_normal_unformatted)
+    }
+
+    toIndexNormalForm(canonical) {
+        return canonical.split('_').map(tk => h.toTitleCase(tk)).join(' ');
     }
 };

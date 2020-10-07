60c32c9bc8d78ed84cfbf0edc7a487ca9ddd20ad
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2020-10-07 11:41:23 +0800

    added "error message" to q!merge (unfinished)

--- commands/merge.js
+++ commands/merge.js
@@ -6,15 +6,24 @@ module.exports = {
     execute(message, args) {
         const merges = require('../jsons/merges.json');
         let tower1 = `${args[0]} ${args[1]}`;
-        let tower2 = `${args[2]} ${args[3]}`;
+        let tower2;
+        if (!args[3]) tower2 = `${args[2]}`;
+        else tower2 = `${args[2]} ${args[3]}`;
+        let res;
         for (let i = 0; i < merges.length; i++) {
             let t1 = merges[i].tower1.toLowerCase();
             let t2 = merges[i].tower2.toLowerCase();
 
             if (t1 == tower1 && t2 == tower2) {
-                message.channel.send(merges[i].link);
+                res = merges[i].link;
                 break;
             }
         }
+        if (!res) {
+            return message.channel.send(
+                'example: q!merge t dart m dart\norder: t path before m path before b path\ntower order/names: dart, boomer, bomb, tack, glue, sniper, sub, boat, ace, heli, mortar, wiz, super, ninja, alch, druid, village, farm, spact, engi\nhero names: quincy, gwen, striker, obyn, churchill, ben, ezili, pat, adora, brickell, etienne'
+            );
+        }
+        return message.channel.send(res);
     },
 };

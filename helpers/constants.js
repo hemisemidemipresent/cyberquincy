c8601343fda2909afd296c92937190fa260a40bd
rmlgaming <linden.cool@gmail.com>
2020-10-28 19:57:04 -0400

    Adding map difficulty modifiers

--- helpers/constants.js
+++ helpers/constants.js
@@ -23,17 +23,24 @@ BASE_HERO_XP_TO_GET_LEVEL = [
 ]
 
 HERO_LEVELING_MODIFIERS = {
-    QUINCY: 1,
-    GWEN: 1,
-    OBYN: 1,
-    JONES: 1,
-    EZILI: 1.425,
-    BEN: 1.5,
-    CHURCHILL: 1.71,
-    PAT: 1.425,
     ADORA: 1.71,
+    BEN: 1.5,
     BRICKELL: 1.425,
+    CHURCHILL: 1.71,
     ETIENNE: 1,
+    EZILI: 1.425,
+    GWEN: 1,
+    JONES: 1,
+    OBYN: 1,
+    PAT: 1.425,
+    QUINCY: 1,
+}
+
+HERO_LEVELING_MAP_DIFFICULTY_MODIFIERS = {
+    BEGINNER: 1,
+    INTERMEDIATE: 1.1,
+    ADVANCED: 1.2,
+    EXPERT: 1.3,
 }
 
 module.exports = {

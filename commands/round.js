c2a8652fd0428e96ce48c2f460da2db3329e6163
rmlgaming <68867396+rmlgaming@users.noreply.github.com>
2021-02-21 12:07:33 -0500

    TMP

--- commands/round.js
+++ commands/round.js
@@ -1,18 +1,27 @@
-const round2 = require('../jsons/round2.json');
-const { cyber } = require('../jsons/colours.json');
-const r1 = require('../jsons/rounds.json');
+const cash = require('../jsons/round2.json');
+const cashAbr = require('../jsons/abrincome.json');
+const colours = require('../jsons/colours.json');
+const rounds = require('../jsons/rounds.json');
 const RoundParser = require('../parser/round-parser');
 const OptionalParser = require('../parser/optional-parser');
 const ModeParser = require('../parser/mode-parser');
 const AnyOrderParser = require('../parser/any-order-parser');
 const json = require('../jsons/rounds_topper.json');
 
-module.exports = {
-    name: 'round',
-    description: 'tells you about the rounds (below 100)',
-    aliases: ['r'],
-    execute(message, args) {
-        let parsed = CommandParser.parse(
+function execute(message, args, originalCommandName) {
+    if (args.length == 0 || (args.length == 1 && args[0] == 'help')) {
+        return module.exports.helpMessage(message);
+    }
+
+    let parsed = null;
+    if (originalCommandName.includes("abr")) {
+        parsed = CommandParser.parse(
+            args,
+            new RoundParser('ALL')
+        );
+        parsed.addField('mode', 'abr')
+    } else {
+        parsed = CommandParser.parse(
             args,
             new AnyOrderParser(
                 new RoundParser('ALL'),
@@ -22,82 +31,101 @@ module.exports = {
                 )
             )
         );
-        if (parsed.hasErrors()) {
-            return module.exports.errorMessage(message, parsed.parsingErrors);
-        }
+    }
+    if (parsed.hasErrors()) {
+        return module.exports.errorMessage(message, parsed.parsingErrors);
+    }
 
-        function getLength(round, arrayOfRounds) {
-            let roundArray = arrayOfRounds[round];
-            let longest = 0;
-            let end = 0;
-            for (i = 0; i < roundArray.length; i++) {
-                end = parseInt(roundArray[i][3]);
-                if (end > longest) {
-                    longest = end;
-                }
+    function getLength(round, arrayOfRounds) {
+        let roundArray = arrayOfRounds[round];
+        let longest = 0;
+        let end = 0;
+        for (i = 0; i < roundArray.length; i++) {
+            end = parseInt(roundArray[i][3]);
+            if (end > longest) {
+                longest = end;
             }
-            return longest / 60; //btd6 is 60fps game
         }
-        function getData(round, arrayOfRounds) {
-            let roundArray = arrayOfRounds[round];
-            let output = '';
-            for (i = 0; i < roundArray.length; i++) {
-                output += `\n${roundArray[i][1]} ${roundArray[i][0]}`;
-            }
-            return output;
-        }
+        return longest / 60; //btd6 is 60fps game
+    }
 
-        let xp = 0;
-        let totalxp = 0;
-        if (parsed.round < 21) {
-            xp = 20 * parsed.round + 20;
-            totalxp =
-                40 +
-                50 * (parsed.round - 1) +
-                10 * Math.pow(parsed.round - 1, 2);
-        } else if (parsed.round > 20 && parsed.round < 51) {
-            xp = 40 * (parsed.round - 20) + 420;
-            totalxp =
-                4600 +
-                440 * (parsed.round - 20) +
-                20 * Math.pow(parsed.round - 20, 2);
-        } else {
-            xp = (parsed.round - 50) * 90 + 1620;
-            totalxp =
-                35800 +
-                1665 * (parsed.round - 50) +
-                45 * Math.pow(parsed.round - 50, 2);
-        }
-        let object = json.reg;
-        let length = getLength(parsed.round, object);
-        let data = getData(parsed.round, object);
-        let sumOfData = r1[`r${parsed.round}`];
-        let rbe = round2[parsed.round].rbe;
-        const roundEmbed = new Discord.MessageEmbed()
-            .setTitle(`round ${parsed.round}`)
-            .setDescription(`${sumOfData}\n{${data}\n}`)
-            .addField('round length', `${Math.round(length * 100) / 100}`, true)
-            .addField('RBE', `${rbe}`, true)
-            .addField('xp earned in that round', `${xp}`, true)
-            .addField(
-                'cash earned in this round',
-                `${round2[parsed.round].csh}`,
-                true
-            )
-            .addField('total xp if you started at round 1', `${totalxp}`)
-            .addField(
-                '**if:**',
-                'you are not in freeplay (then divide xp by 10 for value) AND\n2) you are playing beginner maps (intermediate +10%, advanced +20%, expert +30%)'
-            )
-            .setFooter('for more data on money use q!income')
-            .setColor(cyber);
-        message.channel.send(roundEmbed);
-    },
-    errorMessage(message, parsingErrors) {
-        let errorEmbed = new Discord.MessageEmbed()
-            .setTitle('ERROR')
-            .addField('Likely Cause(s)', parsingErrors.join('\n'))
-            .setColor(cyber);
-        return message.channel.send(errorEmbed);
-    },
+    let xp = 0;
+    let totalxp = 0;
+    if (parsed.round < 21) {
+        xp = 20 * parsed.round + 20;
+        totalxp =
+            40 +
+            50 * (parsed.round - 1) +
+            10 * Math.pow(parsed.round - 1, 2);
+    } else if (parsed.round > 20 && parsed.round < 51) {
+        xp = 40 * (parsed.round - 20) + 420;
+        totalxp =
+            4600 +
+            440 * (parsed.round - 20) +
+            20 * Math.pow(parsed.round - 20, 2);
+    } else {
+        xp = (parsed.round - 50) * 90 + 1620;
+        totalxp =
+            35800 +
+            1665 * (parsed.round - 50) +
+            45 * Math.pow(parsed.round - 50, 2);
+    }
+    let object = parsed.mode == "abr" ? json.alt : json.reg;
+    let length = getLength(parsed.round, object);
+    let roundContents = rounds[`${parsed.mode == "abr" ? 'a' : ''}r${parsed.round}`].split(',').join("\n");
+    let rbe = round2[parsed.round].rbe;
+    const roundEmbed = new Discord.MessageEmbed()
+        .setTitle(`R${parsed.round}` + (parsed.mode == 'abr' ? ' ABR' : ''))
+        .setDescription(`${roundContents}`)
+        .addField('Round Length (seconds)', `${Math.round(length * 100) / 100}`, true)
+        .addField('RBE', `${gHelper.numberWithCommas(rbe)}`, true)
+        .addField(`XP Earned on R${parsed.round}`, `${gHelper.numberWithCommas(xp)}`, true)
+        .addField(
+            `Cash Earned from R${parsed.round}`,
+            `${round2[parsed.round].csh}`,
+            true
+        )
+        .addField('Total XP if You Started on R1', `${totalxp}`)
+        .addField(
+            '**Note:**',
+            ' • If you are in freeplay, the xp value is 1/10 of what is displayed\n' +
+                ' • Map difficulty xp multipliers are {beginner: 1, intermediate 1.1, advanced 1.2, expert 1.3}'
+        )
+        .setFooter('For more data on round incomes use q!income')
+        .setColor(colours["cyber"]);
+    message.channel.send(roundEmbed);
+}
+
+function helpMessage(message) {
+    let helpEmbed = new Discord.MessageEmbed()
+    .setTitle('`q!round/q!abr` HELP')
+    .setDescription('**Round Details (normal/abr)**')
+    .addField(
+        '`q!round <R>`',
+        'Learn about round R'
+    )
+    .addField(
+        '`q!abr <R>` or `q!round <R> abr`',
+        'Learn about round R in ABR mode'
+    )
+    .setColor(colours['black'])
+
+    return message.channel.send(helpEmbed);
+}
+
+function errorMessage(message, parsingErrors) {
+    let errorEmbed = new Discord.MessageEmbed()
+        .setTitle('ERROR')
+        .addField('Likely Cause(s)', parsingErrors.join('\n'))
+        .setColor(colours["red"]);
+    return message.channel.send(errorEmbed);
+}
+
+module.exports = {
+    name: 'round',
+    description: 'tells you about the rounds (below 100)',
+    aliases: ['r', 'abr'],
+    execute,
+    helpMessage,
+    errorMessage,
 };

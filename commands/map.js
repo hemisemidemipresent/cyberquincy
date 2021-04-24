10f112808a3bc3975412cda27aa110567af7cf3f
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2021-04-24 10:52:53 +0800

    added resort map

--- commands/map.js
+++ commands/map.js
@@ -1,7 +1,7 @@
 const { cyber } = require('../jsons/colours.json');
 const gHelper = require('../helpers/general.js');
 const map = require('../jsons/map.json');
-const OrParser = require('../parser/or-parser')
+const OrParser = require('../parser/or-parser');
 const MapParser = require('../parser/map-parser.js');
 const MapDifficultyParser = require('../parser/map-difficulty-parser');
 const { discord } = require('../aliases/misc.json');
@@ -27,58 +27,53 @@ function displayMapInfo(message, name) {
         .addField('Entrances/Exits', `${m.e}`, true)
         //.addField('Line of sight obstructions', `${m.los}`, true)
         .addField('Bug reporting', `report [here](${discord})`, true)
-        .setFooter(
-            'I am Quincy, Evolved from quincy.',
-            'https://vignette.wikia.nocookie.net/b__/images/0/0c/QuincyCyberPortraitLvl20.png/revision/latest/scale-to-width-down/179?cb=20190612022025&path-prefix=bloons'
-        )
         .setColor(cyber);
     message.channel.send(mapEmbed);
 }
 
 function displayMapDifficultyRBS(message, mapDifficulty) {
     const maps = Aliases.allMapsFromMapDifficulty(mapDifficulty);
-    const mapsLengths = maps.map(m => {
+    const mapsLengths = maps.map((m) => {
         // Get all 1 or more path lengths into a flat array
-        const mapLengths = [map[m].len].flat()
+        const mapLengths = [map[m].len].flat();
         // Return the average of the path lengths
-        return mapLengths.reduce((a,b) => a + b, 0) / mapLengths.length;
+        return mapLengths.reduce((a, b) => a + b, 0) / mapLengths.length;
     });
 
-    mapToRbsSorted = gHelper.zip([maps, mapsLengths]).sort(function(a, b) {
+    mapToRbsSorted = gHelper.zip([maps, mapsLengths]).sort(function (a, b) {
         return b[1] - a[1];
-    })
+    });
 
     let infoEmbed = new Discord.MessageEmbed()
         .setTitle(`${gHelper.toTitleCase(mapDifficulty)} Maps Info`)
         .setAuthor('Cyber Quincy')
         .setColor(cyber);
-    
+
     mapToRbsSorted.forEach(([map, mapLength]) => {
         infoEmbed.addField(
-            Aliases.toIndexNormalForm(map), 
+            Aliases.toIndexNormalForm(map),
             mapLength.toFixed(1)
         );
-    })
+    });
 
-    infoEmbed.setFooter('average of all paths with no obstacles removed and no map mechanics')
+    infoEmbed.setFooter(
+        'average of all paths with no obstacles removed and no map mechanics'
+    );
 
-    return message.channel.send(infoEmbed)
+    return message.channel.send(infoEmbed);
 }
 
 function helpMessage(message) {
     let helpEmbed = new Discord.MessageEmbed()
         .setTitle('`q!map` HELP')
-        .addField(
-            '`q!map <map>`',
-            'Map Info'
-        )
+        .addField('`q!map <map>`', 'Map Info')
         .addField(
             '`q!map <map_difficulty>`',
             'RBSs (sorted) of all maps of the given map difficulty'
         )
         .setColor(cyber);
-    
-    return message.channel.send(helpEmbed)
+
+    return message.channel.send(helpEmbed);
 }
 
 function errorMessage(message, parsingErrors) {
@@ -104,18 +99,15 @@ module.exports = {
         }
         let arr = [args[0]];
         const parsed = CommandParser.parse(
-            arr, 
-            new OrParser(
-                new MapParser(),
-                new MapDifficultyParser(),
-            )
+            arr,
+            new OrParser(new MapParser(), new MapDifficultyParser())
         );
         if (parsed.hasErrors()) {
             return errorMessage(message, parsed.parsingErrors);
         }
-        if(parsed.map) {
+        if (parsed.map) {
             return displayMapInfo(message, parsed.map);
-        } else if(parsed.map_difficulty) {
+        } else if (parsed.map_difficulty) {
             return displayMapDifficultyRBS(message, parsed.map_difficulty);
         }
     },

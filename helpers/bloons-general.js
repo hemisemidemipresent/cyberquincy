c86bff45fc6b129814e1b75d80fdb68e9e718936
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-05-11 15:14:56 +0800

    rounds past 120 for `b`

--- helpers/bloons-general.js
+++ helpers/bloons-general.js
@@ -2,12 +2,14 @@ const IMPOPPABLE_ROUNDS = [6, 100];
 const HARD_ROUNDS = [3, 80];
 const MEDIUM_ROUNDS = [1, 60];
 const EASY_ROUNDS = [1, 40];
-const ALL_ROUNDS = [1, 120];
+const PREDET_ROUNDS = [1, 120]; // predetermined rounds
+const ALL_ROUNDS = [1, 1288555]; // will explain later
 
 module.exports = {
     IMPOPPABLE_ROUNDS,
     HARD_ROUNDS,
     MEDIUM_ROUNDS,
     EASY_ROUNDS,
+    PREDET_ROUNDS,
     ALL_ROUNDS,
 };

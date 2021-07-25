448e5c901d568a4e9a1b6b00c4cde2e3ed5938dc
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-07-25 16:40:45 +0800

    minor fixes

---
+++ commands/update.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'update',
+    execute(message) {
+        return message.channel.send(
+            'No one knows.\nNo one knows when the update is going to come out\nNo one knows when they will fix all the bugs\nNo one knows when the next update will come out because NK does not have a schedule'
+        );
+    },
+};

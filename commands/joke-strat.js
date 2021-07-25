9451cca56e9c742891da83b9bad85ffc443ffcbf
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-07-25 16:40:15 +0800

    removed useless desc

--- commands/joke-strat.js
+++ commands/joke-strat.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: 'strat',
     aliases: ['how', 'howtogetgoodtime'],
-    description: 'calculates the health of blimps, even in freeplay',
     execute(message) {
         return message.channel.send(
             "Send faster, pop more Bloons, end more rounds, don't die"

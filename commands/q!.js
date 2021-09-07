20dc9960bb9423e30f930178cbd7c2013b19ac43
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-09-07 15:11:26 +0800

    added q!q!
    you see this lombre

---
+++ commands/q!.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'q!',
+    execute(message) {
+        return message.channel.send(
+            'MUDDY PUDDLES CHIMPS WITH PERMACHANGE AND CHINOOK PERMACHGIANGE AND CHINFOSPK PRJABMNAGHE AND CHOISNOLFOK'
+        );
+    },
+};

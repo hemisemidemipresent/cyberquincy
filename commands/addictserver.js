d293378e6c4c9f79e26fb525326e60fa3900d144
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-10-08 19:22:49 +0800

    q!archmage

--- commands/addictserver.js
+++ commands/addictserver.js
@@ -1,7 +1,9 @@
 module.exports = {
-    name: 'addictserver',
-    alias: ['hentai'],
-    execute(msg) {
-        msg.channel.send('https://discord.gg/RyUTEzbCMr');
+    name: 'archmage',
+    aliases: ['hentai'],
+    async execute(msg) {
+        await msg.channel.send(
+            'https://images-ext-2.discordapp.net/external/zlWTqgBhWyHYdqBskFN2rIsR02yCpjGST2n5CIVaU4k/https/media.discordapp.net/attachments/741369328250912870/895311841113309184/ark_mag_op.png?width=1214&height=683'
+        );
     },
 };

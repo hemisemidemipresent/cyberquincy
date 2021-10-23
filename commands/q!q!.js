ba95b812c5b439fb940d489206ce2c62f64c5e4e
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-10-23 09:40:52 +0800

    q!q!

---
+++ commands/q!q!.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'q!q!',
+    async execute(message) {
+        return await message.channel.send(
+            'i just need to fucking say it. If i put over 1000 hours into something and then replicate every almost every hard thing ever done with it and then go on to do shit thats much much harder it is not egotistical to view myself as the best at it. nor is it egotistical for me to say that there is a chance that something that took me 15 hours using something that i have over 1000 hours of practice in may be the hardest thing ever done in the game. note i have almost never called plorder the hardest thing in the game i usually say “arguably” or “probably” and whenever i have been in an actual civil discussion most of the time people agree that plorder is in fact probably the hardest thing ever done in the game. not to mention that there is no reason for you to argue when i call something harder than something else when the person that did that something else agrees with me.'
+        );
+    },
+};

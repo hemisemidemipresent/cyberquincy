7b9a26f3a6faa0ee1eed47b6029eedca776b793c
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-01-16 08:50:45 +0800

    updated config

--- helpers/config.js
+++ helpers/config.js
@@ -1,8 +1,13 @@
 function activeToken() {
-   const { testing, token, testToken } =  require('../1/config.json');
-   return testing ? testToken : token;
+    const { testing, token, testToken } = require('../1/config.json');
+    return testing ? testToken : token;
+}
+function activeClientID() {
+    const { testing, client, testClient } = require('../1/config.json');
+    return testing ? testClient : client;
 }
 
 module.exports = {
     activeToken,
-}
+    activeClientID
+};
--- register_slash_commands.js
+++ register_slash_commands.js
@@ -1,13 +1,12 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { discordClientId } = require('./1/config.json');
-const { activeToken } = require('./helpers/config');
+const { activeToken, activeClientID } = require('./helpers/config');
 
 slashCommandCenter = require('./slash_command_center');
 commands = slashCommandCenter.commandFiles().map((file) => file.data.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(activeToken());
 
-rest.put(Routes.applicationCommands(discordClientId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+rest.put(Routes.applicationCommands(activeClientID()), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);

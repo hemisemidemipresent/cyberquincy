ae678a4b60b9a034a15240dafa5eafb0953f4d64
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-03-15 21:31:25 +0800

    removed q!round

--- commands/invite.js
+++ commands/invite.js
@@ -1,10 +1,7 @@
 module.exports = {
     name: 'invite',
-
-    aliases: ['add'],
-
     execute(message) {
-        return message.channel.send(
+        return await message.channel.send(
             'https://discordapp.com/oauth2/authorize?client_id=591922988832653313&scope=bot%20applications.commands&permissions=2147863617'
         );
     }

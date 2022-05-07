f49f95e52cce775ce2f11735fb0db5d00793a5a8
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-05-07 12:56:38 +0800

    quiz slashed

---
+++ slash_commands/quiz.js
@@ -0,0 +1,94 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const quiz = require('../jsons/quiz.json');
+const { cyber, orange, turq, magenta } = require('../jsons/colours.json');
+const { shuffle } = require('../helpers/general');
+const { MessageActionRow, MessageButton } = require('discord.js');
+const { discord } = require('../aliases/misc.json');
+const Quiz = require('../helpers/quiz.js');
+let numbers = [0, 1, 2, 3];
+const emojis = [':regional_indicator_a:', ':regional_indicator_b:', ':regional_indicator_c:', ':regional_indicator_d:'];
+
+builder = new SlashCommandBuilder().setName('quiz').setDescription('test your knowledge in all things bloons-related');
+const time = 10000;
+
+async function execute(interaction) {
+    let botMessage = undefined;
+    //const filter = (i) => i.user.id == interaction.author.id;
+
+    await loadQuestion(interaction);
+}
+
+async function loadQuestion(interaction) {
+    item = quiz[Math.floor(Math.random() * quiz.length)];
+    module.exports.item = item;
+    options = item.optns;
+
+    shuffle(numbers);
+
+    const buttons = new MessageActionRow().addComponents(
+        new MessageButton().setCustomId(numbers[0].toString()).setLabel('A').setStyle('PRIMARY'),
+        new MessageButton().setCustomId(numbers[1].toString()).setLabel('B').setStyle('PRIMARY'),
+        new MessageButton().setCustomId(numbers[2].toString()).setLabel('C').setStyle('PRIMARY'),
+        new MessageButton().setCustomId(numbers[3].toString()).setLabel('D').setStyle('PRIMARY')
+    );
+
+    let string = '';
+    for (let i = 0; i < 4; i++) {
+        let rand = numbers[i];
+        string += `${emojis[i]} ${options[rand]}\n`;
+    }
+
+    let QuestionEmbed = new Discord.MessageEmbed()
+        .setTitle(`${item.question}`)
+        .addField('options', string)
+        .addField('please contribute', `join [this server](${discord}) to suggest a question`)
+        .setColor(cyber);
+
+    await interaction.reply({
+        embeds: [QuestionEmbed],
+        components: [buttons]
+    });
+}
+
+async function onButtonClick(interaction) {
+    if (interaction.customId === 'playagain') {
+        return await loadQuestion(interaction);
+    }
+
+    const id = interaction.customId;
+    const userID = interaction.user.id;
+    const item = module.exports.item;
+
+    if (item.ans == id) {
+        Quiz.answerCorrect(userID);
+        let streak = Quiz.getStreak(userID);
+        let correctEmbed = new Discord.MessageEmbed()
+            .setTitle('Congratulations! You got the correct answer!')
+            .setDescription(
+                `${streak}\nNOTE: streak will not be carried over when the bot restarts and that might happen randomly.`
+            )
+            .setColor(turq)
+            .setFooter({ text: 'run /quiz again to play again' });
+        await interaction.update({ embeds: [correctEmbed], components: [] });
+    } else {
+        Quiz.answerWrong(userID);
+        let streak = Quiz.getStreak(userID);
+
+        let wrongEmbed = new Discord.MessageEmbed()
+            .setTitle('Game over! You got the wrong answer!')
+            .setDescription(
+                `${streak}\nNOTE: streak will not be carried over when the bot restarts and that might happen randomly.`
+            )
+            .addField('answer', `||${item.optns[item.ans]}||`)
+            .setColor(orange)
+            .setFooter({ text: 'run /quiz again to play again' });
+
+        await interaction.update({ embeds: [wrongEmbed], components: [] });
+    }
+}
+module.exports = {
+    data: builder,
+    execute,
+    onButtonClick,
+    item: undefined
+};

00f158ed00bd1ba09bb816144c6d94a7b170908f
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-07-10 12:26:45 +0800

    are these even being used???

--- helpers/guilds.js
+++ helpers/guilds.js
@@ -3,7 +3,6 @@ const BTD6_INDEX = '661812833771847700';
 const RACE_SERVER = '543957081183617024';
 const EMOJIS_SERVER = '614111055890612225';
 
-const xp = require('../helpers/xp');
 const { discord } = require('../aliases/misc.json');
 function enterGuild(guild) {
     let channeltosend = guild.channels.cache.find((channel) => channel.name.includes('general') === true);
@@ -26,77 +25,8 @@ function enterGuild(guild) {
     }
 }
 
-async function addMember(member) {
-    if (member.guild.id == CYBER_SUPPORT) {
-        let tag = await Tags.findOne({
-            where: {
-                name: member.id
-            }
-        });
-        // Create db user if it doesn't already exist
-        if (!tag) {
-            tag = await Tags.create({
-                name: member.id,
-                xp: 0,
-                showAds: true,
-                showLevelUpMsg: true,
-                quiz: 0
-            });
-        }
-        let level = xp.xpToLevel(tag.xp);
-        if (level > 3) {
-            await member.roles.add('645126928340353036');
-        }
-        if (level > 10) {
-            await member.roles.add('645629187322806272');
-        }
-    } else if (member.guild.id == BTD6_INDEX) {
-        const wel = new Discord.MessageEmbed()
-            .setTitle('Welcome to the BTD6 Index Discord Server!')
-            .setThumbnail(
-                'https://cdn.discordapp.com/icons/661812833771847700/94e0818cefedd71655d7e4e84a951a37.webp?size=128'
-            )
-            .setDescription(
-                '**remember to read <#661822473246998548>!**\n**Useful external resources can be found in <#661842199679467541>**'
-            )
-
-            .addField(
-                'What is the BTD6 Index?',
-                'The BTD6 Index is a community-maintained spreadsheet that was created for the purpose of compiling resources, documenting challenges, and archiving additional information for BTD6. The goal is to have a vast array of game knowledge all condensed into one area for easy reference and viewing. This post breaks down what each section strives to accomplish as well as addition resources and information that might help you at the game.'
-            )
-            .addField(
-                'related links',
-                '[reddit post](https://www.reddit.com/r/btd6/comments/ejuqcj/official_btd6_index_overview/)\n[BTD6 Index](https://docs.google.com/spreadsheets/d/16of-RFUD1FteVchU9S4vAht39nlh1iraeoNA4u3R9cw/edit?usp=sharing)'
-            )
-            .addField(
-                'Who am I?',
-                `I am a BTD6 Discord bot. Links:\n[invite me to your server](https://discordapp.com/oauth2/authorize?client_id=591922988832653313&scope=bot%20applications.commands&permissions=2147863617),[discord server](${discord})`
-            )
-            .setColor(colours['cyber']);
-        member.send(wel);
-    } else if (member.guild.id == RACE_SERVER) {
-        const general = member.guild.channels.cache.find((channel) => channel.name.includes('general'));
-        general.send(
-            `welcome to the only rAcE sErVer. \nIf you cant get a top 50, you have to read <#667495608155635765> 100 times before entering`
-        );
-    }
-}
-
-async function removeMember(member) {
-    if (member.guild.id == BTD6_INDEX) {
-        let welcome = member.guild.channels.cache.find((channel) => channel.name.includes('welcome'));
-        welcome.send(`**${member.displayName}** got nerfed. hard.`);
-    } else if (member.guild.id == RACE_SERVER) {
-        const general = member.guild.channels.cache.find((channel) => channel.name.includes('general'));
-        general.send(`**${member.displayName}** couldn't resist it and accidentally revealed that he/she is hacking races`);
-    }
-}
-
 module.exports = {
     enterGuild,
-    addMember,
-    removeMember,
-
     CYBER_SUPPORT,
     BTD6_INDEX,
     RACE_SERVER,

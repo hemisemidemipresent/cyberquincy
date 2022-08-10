bb580af1b86efb70a2943005ae3bbc0db392cddc
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-08-10 15:56:07 +0800

    @Tree69420 bruh why is it called invite

--- slash_commands/vtsg.js
+++ slash_commands/vtsg.js
@@ -1,11 +1,11 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-builder = new SlashCommandBuilder().setName('invite').setDescription('Invite Cyber Quincy to your server');
+builder = new SlashCommandBuilder().setName('vtsg').setDescription('Invite Cyber Quincy to your server');
 
 async function execute(interaction) {
     return await interaction.reply({
         content:
-        '555 super monkey has the following buffs compared to a TSG (use q!temple):\n• sunblast buffed: +25d\nall other attacks (including subtowers) buffed: ×2d (applied after additive buffs)'
+            '555 super monkey has the following buffs compared to a TSG (use q!temple):\n• sunblast buffed: +25d\nall other attacks (including subtowers) buffed: ×2d (applied after additive buffs)'
     });
 }
 

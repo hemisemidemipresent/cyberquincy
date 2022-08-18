e30f7dcc44b4af7c95bd09aa53536ca0b15e8c07
hemisemidemipresent <hnngggrrrr@gmail.com>
2022-08-18 09:20:28 +0800

    ephemeral

--- slash_commands/tag.js
+++ slash_commands/tag.js
@@ -1,82 +1,73 @@
 const { parse } = require('@ltd/j-toml');
-const { SlashCommandBuilder, Collection, Formatters, EmbedBuilder } = require('discord.js');
+const { Collection, EmbedBuilder, Formatters, SlashCommandBuilder } = require('discord.js');
 const { readFileSync } = require('fs');
 const { cyber } = require('../jsons/colours.json');
-const { templates } = require('../tags/templates');
-
 const cache = populateTagCache();
 
 builder = new SlashCommandBuilder()
-	.setName('tag')
-	.setDescription('Display a tag by its name or alias')
-	.addStringOption((option) =>
-		option
-			.setName('query')
-			.setDescription('The tag name or alias')
-			.setAutocomplete(true)
-			.setRequired(true)
-	);
+    .setName('tag')
+    .setDescription('Display a tag by its name or alias')
+    .addStringOption((option) =>
+        option.setName('query').setDescription('The tag name or alias').setAutocomplete(true).setRequired(true)
+    );
 
 function execute(interaction) {
-	const query = interaction.options.getString('query').toLowerCase();
-	const tag = cache.get(query);
-	const options = {};
+    const query = interaction.options.getString('query').toLowerCase();
+    const tag = cache.get(query);
+    const options = {};
 
-	if (tag) {
-		options.content = tag.content;
-		options.embeds = tag.embeds;
-	} else options.content ??= `Couldn't find a tag with the name ${Formatters.bold(query)}.`;
+    if (tag) {
+        options.content = tag.content;
+        options.embeds = tag.embeds;
+    } else {
+        options.content ??= `Couldn't find a tag with the name ${Formatters.bold(query)}.`;
+        options.ephemeral = true;
+    }
 
-	return interaction.reply(options);
+    return interaction.reply(options);
 }
 
 function onAutocomplete(interaction) {
-	const query = interaction.options.getFocused().toLowerCase();
+    const query = interaction.options.getFocused().toLowerCase();
 
-	if (!query) {
-		const featured = cache.firstKey(25).map((name) => ({
-			name,
-			value: name,
-		}));
+    if (!query) {
+        const featured = cache.firstKey(25).map((name) => ({
+            name,
+            value: name
+        }));
 
-		return interaction.respond(featured);
-	}
+        return interaction.respond(featured);
+    }
 
-	const tags = cache.filter(({ keywords }) => keywords.some((kw) => kw.includes(query)));
+    const tags = cache.filter(({ keywords }) => keywords.some((kw) => kw.includes(query)));
 
-	const responseArr = tags.map(({ keywords }) => ({
-		name: keywords.at(-1),
-		value: keywords.at(-1),
-	}));
+    const responseArr = tags.map(({ keywords }) => ({
+        name: keywords.at(-1),
+        value: keywords.at(-1)
+    }));
 
-	return interaction.respond(responseArr);
-}
-
-function resolveTemplateTags(str) {
-	return str.replace(/{(\w+)}/g, (match, name) => templates[name] ?? match);
+    return interaction.respond(responseArr);
 }
 
 function populateTagCache() {
-	const file = resolveTemplateTags(readFileSync('./tags/tags.toml', 'utf8'));
-	const data = parse(file, '\n');
-	const cache = new Collection();
+    const file = readFileSync('./tags/tags.toml', 'utf8');
+    const data = parse(file, '\n');
+    const cache = new Collection();
 
-	for (const [key, value] of Object.entries(data)) {
-		value.embeds?.forEach(
-			(embed, i) => (value.embeds[i] = new EmbedBuilder(embed).setColor(embed.color ?? cyber))
-		);
+    for (const [key, value] of Object.entries(data)) {
+        value.embeds?.forEach((embed, i) => (value.embeds[i] = new EmbedBuilder(embed).setColor(embed.color ?? cyber)));
 
-		value.content ??= '';
+        value.content ??= '';
 
-		value.keywords?.push(key) ?? (value.keywords = [key]);
-		cache.set(key, value);
-	}
+        value.keywords?.push(key) ?? (value.keywords = [key]);
+        cache.set(key, value);
+    }
 
-	return cache;
+    return cache;
 }
 
 module.exports = {
-	data: builder,
-	execute,
-	onAutocomplete,
+    data: builder,
+    execute,
+    onAutocomplete
 };

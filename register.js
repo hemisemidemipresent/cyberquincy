29436784d15be2068de7797f86742c12fe9d35cf
ToastOnAStick <64027609+ToasterSticks@users.noreply.github.com>
2022-08-24 16:27:48 +0000

    feat(cmd): global ephemeral option

--- register.js
+++ register.js
@@ -3,11 +3,38 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { activeToken, activeClientID } = require('./helpers/config');
 const { testing, testingGuild } = require('./1/config.json');
+const { ApplicationCommandOptionType } = require('discord.js');
 
 async function loadAliases() {
     const AliasRepository = require('./alias-repository.js');
     global.Aliases = new AliasRepository();
     await Aliases.asyncAliasFiles();
+}
+
+function addHideOptions(options) {
+    const hasSubcommand = options.some(
+        ({ type }) =>
+            type === ApplicationCommandOptionType.SubcommandGroup ||
+            type === ApplicationCommandOptionType.Subcommand
+    );
+
+    if (hasSubcommand)
+        options.forEach((option) => ((option.options ??= []), addHideOptions(option.options)));
+    else
+        options.push({
+            name: 'hide',
+            description: 'Whether to hide the response',
+            type: ApplicationCommandOptionType.Boolean,
+        });
+}
+
+function getCommandBody(file) {
+    const body = file.data.toJSON();
+    body.options ??= [];
+
+    addHideOptions(body.options);
+
+    return body;
 }
 
 async function registerCommands() {
@@ -20,7 +47,7 @@ async function registerCommands() {
         const commands = slashCommandCenter
             .commandFiles()
             //.filter((file) => !file.beta)
-            .map((file) => file.data.toJSON());
+            .map(getCommandBody);
         await rest
             .put(Routes.applicationGuildCommands(activeClientID(), testingGuild), { body: commands })
             .then(() => console.log('Successfully registered application commands for test guild.'))
@@ -29,7 +56,7 @@ async function registerCommands() {
         const commands = slashCommandCenter
             .commandFiles()
             .filter((file) => !file.beta)
-            .map((file) => file.data.toJSON());
+            .map(getCommandBody);
         await rest
             .put(Routes.applicationCommands(activeClientID()), { body: commands })
             .then(() => console.log('Successfully registered application commands for all guilds.'))
@@ -38,7 +65,7 @@ async function registerCommands() {
         const betaCommands = slashCommandCenter
             .commandFiles()
             .filter((file) => file.beta)
-            .map((file) => file.data.toJSON());
+            .map(getCommandBody);
         await rest
             .put(Routes.applicationGuildCommands(activeClientID(), testingGuild), { body: betaCommands })
             .then(() => console.log('Successfully registered beta commands for test guild.'))
--- slash_command_center.js
+++ slash_command_center.js
@@ -62,11 +62,40 @@ async function handleSelectMenu(interaction) {
     }
 }
 
+function extendStructure(_class) {
+    const prototype = _class.prototype;
+
+    const deferReply = prototype.deferReply;
+    prototype.deferReply = async function (options = {}) {
+        options.ephemeral = this.options.getBoolean('hide') || Boolean(options.ephemeral);
+
+        return deferReply.call(this, options);
+    };
+
+    const reply = prototype.reply;
+    prototype.reply = async function (options) {
+        const ephemeral = this.options.getBoolean('hide') || Boolean(options.ephemeral);
+        options = {
+            ...(typeof options === 'object' ? options : { content: options }),
+            ephemeral,
+        };
+
+        const message = await (this.deferred || this.replied
+            ? this.followUp(options)
+            : reply.call(this, options));
+
+        this.replied = true;
+
+        return message;
+    };
+};
+
 module.exports = {
     commandFiles,
     configureCommands,
     handleCommand,
     handleButton,
     handleAutocomplete,
-    handleSelectMenu
+    handleSelectMenu,
+    extendStructure
 };

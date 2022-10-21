7aa7c39bd1c581fc71762f278839823667c1bf7e
blackeyefly <rayofdoombloons@gmail.com>
2022-10-21 15:48:06 -0400

    Fix rounding

--- helpers/bloons-general.js
+++ helpers/bloons-general.js
@@ -6,9 +6,7 @@ const PRICE_MULTS = {
 }
 
 function difficultyPriceMult(mediumCost, difficulty) {
-    return Math.round((mediumCost * PRICE_MULTS[difficulty]) / 5) * 5;
+    // The game rounds up if mediumCost * PRICE_MULTS[difficulty] ends in 7.5 and down if it ends in 2.5
+    let round = (mediumCost * PRICE_MULTS[difficulty] * 10) % 100 == 25 ? Math.floor : Math.round;
+    return round((mediumCost * PRICE_MULTS[difficulty]) / 5) * 5;
 }
-
-module.exports = {
-    difficultyPriceMult,
-};

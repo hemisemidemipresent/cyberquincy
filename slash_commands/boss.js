3ba372867f49f1c16b051ca57318ca074ca506c7
Ethan Lawrence <28376972+EthanLawr@users.noreply.github.com>
2023-05-15 17:17:05 -0400

    Update boss.js
    Condense

--- slash_commands/boss.js
+++ slash_commands/boss.js
@@ -1,74 +1,59 @@
-const { SlashCommandBuilder } = require('discord.js');
-const gHelper = require('../helpers/general.js');
+const { SlashCommandBuilder } = require('discord.js'),
+  gHelper = require('../helpers/general.js');
 
-const { Bloonarius } = require('../jsons/boss.json');
-const { Lych } = require('../jsons/boss.json');
-const { Vortex } = require('../jsons/boss.json');
+const { Bloonarius, Lych, Vortex } = require('../jsons/boss.json');
 
 const builder = new SlashCommandBuilder()
-    .setName('boss')
-    .setDescription('Find information for boss blimps')
-    .addStringOption((option) =>
-        option
-            .setName('name')
-            .setDescription('Which boss it is')
-            .addChoices(
-                { name: 'Bloonarius', value: 'bloonarius' },
-                { name: 'Gravelord Lych', value: 'lych' },
-                { name: 'Vortex: Deadly Master of Air', value: 'vortex' }
-            )
-            .setRequired(true)
-    )
-    .addIntegerOption((option) => option.setName('tier').setDescription('Tier of the boss').setRequired(true))
-    .addBooleanOption((option) =>
-        option.setName('elite').setDescription('Whether the boss is elite or not').setRequired(true)
-    );
+  .setName('boss')
+  .setDescription('Find information for boss blimps')
+  .addStringOption(option =>
+    option
+      .setName('name')
+      .setDescription('Which boss it is')
+      .addChoices(
+        { name: 'Bloonarius', value: 'bloonarius' },
+        { name: 'Gravelord Lych', value: 'lych' },
+        { name: 'Vortex: Deadly Master of Air', value: 'vortex' },
+      )
+      .setRequired(true),
+  )
+  .addIntegerOption(option =>
+    option.setName('tier').setDescription('Tier of the boss').setRequired(true).setMaxValue(5).setMinValue(1))
+  .addBooleanOption(option =>
+    option.setName('elite').setDescription('Whether the boss is elite or not').setRequired(true),
+  );
 
-function validateInput(interaction) {
-    const tier = interaction.options.getInteger('tier');
-    if (tier < 1 || tier > 5) return 'Invalid Boss tier!';
-}
+function execute(interaction) {
+  const tier = interaction.options.getInteger('tier'),
+    boss = interaction.options.getString('name'),
+    isElite = interaction.options.getBoolean('elite');
 
-async function execute(interaction) {
-    const validationFailure = validateInput(interaction);
-    if (validationFailure)
-        return await interaction.reply({
-            content: validationFailure,
-            ephemeral: true
-        });
-    const tier = interaction.options.getInteger('tier');
-    const boss = interaction.options.getString('name');
-    const isElite = interaction.options.getBoolean('elite');
-
-    let embed;
-    switch (boss) {
-        case 'bloonarius':
-            embed = process(Bloonarius, 'Bloonarius', tier, isElite);
-            break;
-        case 'lych':
-            embed = process(Lych, 'Lych', tier, isElite);
-            break;
-        case 'vortex':
-            embed = process(Vortex, 'Vortex', tier, isElite);
-            break;
-    }
-    return await interaction.reply({ embeds: [embed] });
+  let embed;
+  switch (boss) {
+    case 'bloonarius':
+      embed = process(Bloonarius, 'Bloonarius', tier, isElite);
+      break;
+    case 'lych':
+      embed = process(Lych, 'Lych', tier, isElite);
+      break;
+    case 'vortex':
+      embed = process(Vortex, 'Vortex', tier, isElite);
+      break;
+  }
+  return interaction.reply({ embeds: [embed] });
 }
 
 function process(data, name, tier, isElite) {
-    let embed = new Discord.EmbedBuilder().setTitle(`${isElite ? 'Elite' : ''} ${name} Tier ${tier}`);
-    let desc = data.desc + '\n' + isElite ? data.eliteDesc : normalDesc;
-    let obj = isElite ? data.elite[tier - 1] : data.normal[tier - 1];
-    desc += `\n\n**Stats**:
+  const obj = isElite ? data.elite[tier - 1] : data.normal[tier - 1];
+  let desc = `${data.desc}\n${isElite ? data.eliteDesc : normalDesc}\n\n**Stats**:
 Health: ${gHelper.numberWithCommas(obj.hp)}
 Speed: ${obj.speed} rbs/s
 ${obj.desc}
 \`\`\`${data.customRounds}\`\`\``;
-    embed.setDescription(desc);
-    return embed;
+  return new Discord.EmbedBuilder().setTitle(`${isElite ? 'Elite' : ''} ${name} Tier ${tier}`).setDescription(desc);
 }
 
 module.exports = {
-    data: builder,
-    execute
+  data: builder,
+  execute,
 };

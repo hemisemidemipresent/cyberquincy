b62e1e2ad40f70b8341f60931679d295b785c8b6
Landmaster <python0429@gmail.com>
2023-06-25 12:54:57 -0400

    improve display and exclude t5 handlers

--- slash_commands/adorasac.js
+++ slash_commands/adorasac.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { isTower, allTowers, allUpgradeCrosspathSets, costOfTowerUpgradeSet } = require('../helpers/towers.js');
+const { red, cyber } = require('../jsons/colors.json');
 
 const INF_BAG_SZ = 1000000;
 const XP_CAP = 400000;
@@ -30,8 +31,14 @@ async function execute(interaction) {
     try {
         let excludedTowers = parseExcludedTowers(interaction.options.getString('excluded_towers'));
 
+        let xp = interaction.options.getInteger('xp');
+
+        if (xp < 0 || xp > XP_CAP) {
+            throw new RangeError(`Provided XP value ${xp} should be between 0 and ${XP_CAP} inclusive`);
+        }
+
         // optimization: use money divided by 5 to shrink DP space as all tower/upgrade costs are divisble by 5
-        let moneyToSpendDiv5 = Math.ceil(interaction.options.getInteger('xp') / (4 * 5));
+        let moneyToSpendDiv5 = Math.ceil(xp / (4 * 5));
 
         // set up DP arrays
         let bagSize = new Array(XP_CAP_ADJUSTED+1);
@@ -45,8 +52,11 @@ async function execute(interaction) {
         for (let tower of allTowers()) {
             if (excludedTowers.indexOf(tower) < 0) {
                 for (let xpathSet of allUpgradeCrosspathSets()) {
-                    let processedCost = Math.ceil(costOfTowerUpgradeSet(tower, xpathSet, 'hard') / 5);
-                    costsDiv5ToUpgrade.set(processedCost, `${tower}#${xpathSet}`);
+                    // exclude t5 beast handlers due to merge requirements
+                    if (tower !== 'beast_handler' || xpathSet.indexOf('5') < 0) {
+                        let processedCost = Math.ceil(costOfTowerUpgradeSet(tower, xpathSet, 'hard') / 5);
+                        costsDiv5ToUpgrade.set(processedCost, `${tower}#${xpathSet}`);
+                    }
                 }
             }
         }
@@ -60,9 +70,11 @@ async function execute(interaction) {
             }
         }
 
+        let resultCost = 0;
         let result = [];
         for (let i = moneyToSpendDiv5; i <= XP_CAP_ADJUSTED; ++i) {
             if (bagSize[i] !== INF_BAG_SZ) {
+                resultCost = i * 5;
                 while (i > 0) {
                     result.push(costsDiv5ToUpgrade.get(lastItem[i]));
                     i -= lastItem[i];
@@ -72,14 +84,21 @@ async function execute(interaction) {
         }
 
         return await interaction.reply({
-            content: JSON.stringify(result),
-            ephemeral: true
+            embeds: [
+                new Discord.EmbedBuilder()
+                .setColor(cyber)
+                .setTitle(`${result.length} tower(s) with cost $${resultCost}`)
+                .setDescription(result.join('\n'))
+            ]
         });
     } catch (error) {
         if (error instanceof RangeError) {
             return await interaction.reply({
-                content: error.message,
-                ephemeral: true
+                embeds: [
+                    new Discord.EmbedBuilder()
+                    .setColor(red)
+                    .setTitle(error.message)
+                ]
             });
         } else {
             throw error;

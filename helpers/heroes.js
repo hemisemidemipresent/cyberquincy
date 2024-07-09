9b12a2e74a25ed57095eb0e7ca330959ad601f7f
hemisemidemipresent <hnngggrrrr@gmail.com>
2024-07-09 22:20:08 +0800

    bruh

--- helpers/heroes.js
+++ helpers/heroes.js
@@ -60,8 +60,7 @@ function accumulatedXpCurve(
         }
 
         energizerFactor = round >= energizerAcquiredRound ? 1.5 : 1;
-        // mkFactor = mk ? 1.1 * 1.05 * 1.08 : 1;
-        mkFactor = 1;
+        mkFactor = mk ? 1.1 * 1.05 * 1.08 : 1;
 
         if (round == startingRound - 1) {
             xpGains.push(0);

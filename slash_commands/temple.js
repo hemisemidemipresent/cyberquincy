d65ff49807445ad36ee04956b9fdffc8b3c9eb37
SciplyPandora <pandora.sciply@gmail.com>
2025-06-02 15:46:59 +1200

    /temple refactor

--- slash_commands/temple.js
+++ slash_commands/temple.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { isValidTempleSet } = require('../helpers/towers');
-
-const t = require('../jsons/temple.json');
-const t2 = require('../jsons/temple2.json');
+const { templeBloonology } = require('../helpers/bloonology');
 
 const { yellow } = require('../jsons/colors.json');
 
@@ -12,7 +10,7 @@ builder = new SlashCommandBuilder()
     .addSubcommand((subcommand) =>
         subcommand
             .setName('by_category')
-            .setDescription('Input the amount of money you sacrificed into the temple (you can get this with `/calc`)') //
+            .setDescription('Input the amount of money you sacrificed to the temple (you can get this with `/calc`)')
             .addIntegerOption((option) =>
                 option.setName('primary').setDescription('Total amount of money in PRIMARY towers sacrificed')
             )
@@ -23,7 +21,7 @@ builder = new SlashCommandBuilder()
                 option.setName('magic').setDescription('Total amount of money in MAGIC towers sacrificed')
             )
             .addIntegerOption((option) =>
-                option.setName('support').setDescription('amt of money in SUPPORT towers sacrificed')
+                option.setName('support').setDescription('Total amount of money in SUPPORT towers sacrificed')
             )
     )
     .addSubcommand((subcommand) =>
@@ -34,9 +32,31 @@ builder = new SlashCommandBuilder()
                 option
                     .setName('temple_set')
                     .setDescription(
-                        'temple with 3 level-9 sacrifices is 1110 if support was skipped, 1101 if magic was skipped, etc'
+                        'Temple with 3 level-9 sacrifices is 1110 if support was skipped, 1101 if magic was skipped, etc'
                     )
                     .setRequired(true)
+            )
+            .addStringOption((option) =>
+                option
+                    .setName('temple_upgrade')
+                    .setDescription('The temple upgrade for the base stats')
+                    .addChoices(
+                        { name: '400', value: '400' },
+                        { name: '410', value: '410' },
+                        { name: '420', value: '420' },
+                        { name: '401', value: '401' },
+                        { name: '402', value: '402' },
+                        { name: '500', value: '500' },
+                        { name: '510', value: '510' },
+                        { name: '520', value: '520' },
+                        { name: '501', value: '501' },
+                        { name: '502', value: '502' }
+                    )
+            )
+            .addBooleanOption((option) =>
+                option
+                    .setName('vtsg')
+                    .setDescription('Use the stats of a Vengeful True Sun God?')
             )
     );
 
@@ -52,37 +72,31 @@ async function by_category(interaction) {
     let support = interaction.options.getInteger('support') || 0;
 
     let embed = new Discord.EmbedBuilder();
-    embed.setTitle('Temple stats');
     embed.setColor(yellow);
 
-    let fields = [];
-    let inputs = [primary, military, magic, support];
-    let names = ['Primary', 'Military', 'Magic', 'Support'];
-    for (let i = 0; i < 4; i++) {
-        let level = cashToLevel(inputs[i]);
-        if (level == 0) continue;
-        fields.push({ name: `${names[i]} sacrifice ($${inputs[i]} - level ${level}/9)`, value: t[i][level - 1] });
+    let desc = templeBloonology('400', primary, military, magic, support);
+    if (desc === '') {
+        embed.setDescription(
+            'No changes from the base temple due to insufficient sacrifices to meet the threshold'
+        );
+    } else {
+        embed.setDescription(desc + '\n\nThis command only tells you the **changes** from the base temple due to sacrifices. For the base temple stats, please use `/tower`');
     }
-    if (fields.length > 0) {
-        embed.setDescription(
-            'This command only tells you the **changes** from the temple due to sacrifices. For the base temple with no sacrifices, please use `/tower` with the temple crosspath of choice'
-        );
-        embed.addFields(fields);
-    } else
-        embed.setDescription(
-            'no changes from due to sacrifices; not enough sacrificed\nFind the base temple stats with `/tower` with the temple crosspath of choice'
-        );
-    embed.setFooter({ text: 'Cash thresholds for levels are: 300, 1000, 2000, 4000, 7500, 10000, 15000, 25000, 50000' });
+    embed.setFooter({ text: 'Cash thresholds for levels are: 300, 1001, 2001, 4001, 7501, 10001, 15001, 25001, 50001' });
+
     return await interaction.reply({ embeds: [embed] });
 }
 
 async function max_temple(interaction) {
     let temple_set = interaction.options.getString('temple_set');
-    if (!isValidTempleSet(temple_set))
+    let temple_upgrade = interaction.options.getString('temple_upgrade');
+    let is_vtsg = interaction.options.getBoolean('vtsg') || false;
+
+    if (!isValidTempleSet(temple_set)) {
         return await interaction.reply(
             `Please enter a valid temple set!
             
-            This command is for the statistics of a **maxed temple sacrifice**, i.e. the sacrifices **exceed** $50000.
+            This command is for the statistics of a **maxed temple sacrifice**, i.e. the sacrifices **exceed** $50,000.
             The **temple set** follows a format of \`<primary>/<military>/<magic>/<support>\`. So for example if you have max sacrifices for only primary and magic, your temple set will be \`1/0/1/0\` (this is usually shortened by removing the slashes to \`1010\`)
 
             Although this command accepts the temple set of \`1/1/1/1\` or \`1111\`, when sacrificing towers to a Sun Temple, only three categories count. If four categories are sacrificed then the cheapest is ignored.
@@ -91,17 +105,25 @@ async function max_temple(interaction) {
             So for example if you had a \`1101\` temple that u max-sacrificed on all 4 categories, you would get \`1101\` + \`1111\` = \`2212\`
             `
         );
+    }
+
     let templeSet = temple_set
         .replace(/\/+/g, '')
         .split('')
         .map((x) => parseInt(x));
+    let sacrifices;
+    if (templeSet.includes(2)) {
+        temple_upgrade = temple_upgrade || '500';
+        sacrifices = [...templeSet.map(x => x === 2 ? 50001 : 0), ...templeSet.map(x => x >= 1 ? 50001 : 0)];
+    } else {
+        temple_upgrade = temple_upgrade || '400';
+        sacrifices = templeSet.map(x => x === 1 ? 50001 : 0);
+    }
 
+    let desc = templeBloonology(temple_upgrade, ...sacrifices, vtsg = is_vtsg);
     let embed = new Discord.EmbedBuilder().setColor(yellow);
     embed.setTitle(temple_set);
-
-    for (let i = 0; i < 4; i++) {
-        addSacrificeStats(embed, templeSet[i], i);
-    }
+    embed.setDescription(desc + '\n\nThis command only tells you the **changes** from the base temple due to sacrifices. For the base temple stats, please use `/tower`');
     await interaction.reply({ embeds: [embed] });
 }
 
@@ -109,28 +131,3 @@ module.exports = {
     data: builder,
     execute
 };
-
-function addSacrificeStats(embed, num, i) {
-    const titles = ['Primary sacrifice', 'Military sacrifice', 'Magic sacrifice', 'Support sacrifice'];
-
-    if (num == 0) return;
-    if (num == 1) {
-        return embed.addFields([{ name: titles[i], value: `${t[i][8]}` }]);
-    }
-    if (num == 2) {
-        return embed.addFields([{ name: titles[i], value: `${t[i][8]}\n\n-----True Sun God-----\n${t2[i]}` }]);
-    }
-}
-
-/**
- * input cash, returns a number from 0 - 9 about the temple's sacrifice level.
- * @param {int} cash
- * @returns {int}
- */
-function cashToLevel(cash) {
-    let sacrifice_levels = [300, 1000, 2000, 4000, 7500, 10000, 15000, 25000, 50000];
-    for (let i = 0; i < 9; i++) {
-        if (cash < sacrifice_levels[i]) return i;
-    }
-    return 9;
-}

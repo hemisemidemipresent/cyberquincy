3467694821b7011a88eb9d431a8ffbf9ee572c99
hemisemidemipresent <hnngggrrrr@gmail.com>
2025-06-14 21:43:06 +0800

    /2tc individual alt combos no longer show OG date

--- slash_commands/index-2tc.js
+++ slash_commands/index-2tc.js
@@ -197,9 +197,8 @@ async function displayCombos(interaction, resJson, parsed, searchParams) {
         });
     }
 
+    // Only one combo found
     if (combos.length == 1) {
-
-        // Only one combo found
 
         let challengeEmbed = new Discord.EmbedBuilder()
             .setTitle(embedTitle(parsed, combos))
@@ -207,10 +206,12 @@ async function displayCombos(interaction, resJson, parsed, searchParams) {
 
         const combo = combos[0];
 
+        console.log(`Displaying 2TC combo: ${JSON.stringify(combo)}`);
+
         // i give up I'm hardcoding the fields to use for displaying a single combo ~hemi
-        let fields = ['tower1', 'tower2', 'map', 'date', 'person'];
-        // only OG combos have version infomation
-        if (combo.og) fields.push('version');
+        let fields = ['tower1', 'tower2', 'map', 'person'];
+        // only OG combos have version and date infomation
+        if (combo.og) fields.push('version', 'date');
 
         fields.forEach((field) => challengeEmbed.addFields([{ name: gHelper.toTitleCase(field), value: combo[field], inline: true }]));
 

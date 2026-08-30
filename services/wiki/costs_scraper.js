15772bb6c53f40a8d6b471585d17b58d1b39b485
hemisemidemipresent <hnngggrrrr@gmail.com>
2026-08-30 16:13:50 +0800

    fix costs_scraper.js to include paragons too

--- services/wiki/costs_scraper.js
+++ services/wiki/costs_scraper.js
@@ -13,6 +13,7 @@ async function scrapeCosts() {
 
     // for whatever reason fs operates at the base on our directory
     let costs = JSON.parse(fs.readFileSync("./jsons/costs.json"));
+    let paragonCosts = {}
     const paths = {
         1: "top_path",
         2: "middle_path",
@@ -21,31 +22,44 @@ async function scrapeCosts() {
     upgrades.forEach((upgrade) => {
         const obj = upgrade.title;
         const towerName = Aliases.toAliasNormalForm(obj.tower);
+        if (!costs[towerName]) costs[towerName] = {}
+        if (!costs[towerName].upgrades) {
+            costs[towerName].upgrades = {
+                "top_path": {},
+                "middle_path": {},
+                "bottom_path": {},
+            }
+        }
+        // paragon
+        if (obj.path == -1 && obj.tier == 6) {
+            paragonCosts[towerName] = parseInt(obj.cost)
+            return
+        }
         const path = paths[parseInt(obj.path)];
-        if (costs[towerName]) costs[towerName].upgrades[path][obj.tier] = parseInt(obj.cost);
+        costs[towerName].upgrades[path][obj.tier] = parseInt(obj.cost);
     });
     fs.writeFileSync("./jsons/costs.json", JSON.stringify(costs, null, 4));
-}
-
-async function scrapeParagonCosts() {
-    // const response = await fetch("https://www.bloonswiki.com/api.php?action=cargoquery&tables=btd6_paragons&fields=tower,name,cost,icon&format=json");
-    const response = await fetch(
-        "https://www.bloonswiki.com/api.php?action=cargoquery&tables=btd6_paragons&fields=tower,cost&format=json",
-    );
-    if (!response.ok) return console.warn("paragon cost scraping failed");
-    const rawJson = await response.json();
-    const paragons = rawJson?.cargoquery;
-    let paragonCosts = JSON.parse(fs.readFileSync("./jsons/paragon_costs.json"));
-    paragons.forEach((paragon) => {
-        paragon = paragon.title;
-        paragonCosts[Aliases.toAliasNormalForm(paragon.tower)] = parseInt(paragon.cost);
-    });
     fs.writeFileSync("./jsons/paragon_costs.json", JSON.stringify(paragonCosts, null, 4));
-
-    // https://www.bloonswiki.com/api.php?action=query&prop=imageinfo&format=json&titles=File:BTD6%20BombShooter%20BombParagonIcon.png&iiprop=url
 }
+
+// async function scrapeParagonCosts() {
+//     // const response = await fetch("https://www.bloonswiki.com/api.php?action=cargoquery&tables=btd6_paragons&fields=tower,name,cost,icon&format=json");
+//     const response = await fetch(
+//         "https://www.bloonswiki.com/api.php?action=cargoquery&tables=btd6_paragons&fields=tower,cost&format=json",
+//     );
+//     if (!response.ok) return console.warn("paragon cost scraping failed");
+//     const rawJson = await response.json();
+//     const paragons = rawJson?.cargoquery;
+//     let paragonCosts = JSON.parse(fs.readFileSync("./jsons/paragon_costs.json"));
+//     paragons.forEach((paragon) => {
+//         paragon = paragon.title;
+//         paragonCosts[Aliases.toAliasNormalForm(paragon.tower)] = parseInt(paragon.cost);
+//     });
+//     fs.writeFileSync("./jsons/paragon_costs.json", JSON.stringify(paragonCosts, null, 4));
+
+//     // https://www.bloonswiki.com/api.php?action=query&prop=imageinfo&format=json&titles=File:BTD6%20BombShooter%20BombParagonIcon.png&iiprop=url
+// }
 
 module.exports = {
     scrapeCosts,
-    scrapeParagonCosts,
 };
